32bdb1887ba13b4ea0c98345f7710e98c96ff6e2
abuba0a <168736244+abuba0a@users.noreply.github.com>
2024-05-13 07:12:28 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,15 @@
+from random import randint
+
+key_ = randint(3, 20)
+print('n - ', key_)
+result = []
+
+for x in range(1, 21):
+    for y in range(x + 1, 21):
+        c = x + y
+        if key_ % c == 0:
+            result.append(x)
+            result.append(y)
+        continue
+
+print('result - ', *result)
